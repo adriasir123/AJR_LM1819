c2c546768dea9d0a6f88d67a13e8e5f3abeecf18
Alumno <adristudy@gmail.com>
2019-02-14 09:18:33 +0100

    asdas

---
+++ php/ejercicio05/ejercicios.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ejercicios</title>
+</head>
+<body>
+    <?php
+        // function pase nombre de fichero y devuelva su extensión fichero y la devuelve en mayúscula
+        print "holaa";
+    ?>
+</body>
+</html>
