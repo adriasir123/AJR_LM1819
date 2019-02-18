adb6692eceb5c3965038c91023d9a4c790e1ca68
unknown <adristudy@gmail.com>
2019-02-18 23:31:20 +0100

    asd

--- php/php_ejercicios/ej1.php
+++ php/php_ejercicios/ej1.php
@@ -1,8 +1,11 @@
 <?php
-
+    //Realice un formulario que introduzca dos valores (pies y pulgadas) y los convierta a
+    //centímetros. Los pies deben ser un número entero mayor o igual que cero. Las pulgadas
+    //son un número entero o decimal mayor o igual que cero. Un pie son doce pulgadas y una
+    //pulgada son 2,54 cm.
     $nombre = trim(htmlspecialchars(strip_tags($_REQUEST["nombre"]), ENT_QUOTES, "UTF-8"));
 
-    
+
     if ($nombre != "") {
         print "<p>Su nombre es $nombre</p>";
     }
