ce16d7d4d47c94dbdfe41ce80498b0da586e15dd
Alumno <adristudy@gmail.com>
2019-02-18 14:41:55 +0100

    asdasd

--- php/ejercicio06/resultado.php
+++ php/ejercicio06/resultado.php
@@ -1,6 +1,10 @@
 <?php
-    if ($_REQUEST["nombre"]!="") {
-        print "<p>Su nombre es $_REQUEST[nombre]</p>";
+
+    $nombre = trim(htmlspecialchars(strip_tags($_REQUEST["nombre"]), ENT_QUOTES, "UTF-8")); //strip_tags QUITA ETIQUETAS HTML 
+            //Trim sirve para quitar espacios por delante y por detrás
+            //quita espacios, ampersan, doble comillas, quita etiquetas html
+    if ($nombre != "") {
+        print "<p>Su nombre es $nombre</p>";
     }
 
     if (isset($_REQUEST["opcion"])) {
