059788336986038cf942cbd9582e9571cff87c1b
unknown <adristudy@gmail.com>
2019-02-18 23:24:30 +0100

    asd

--- php/ejs_clase/form2.php
+++ php/ejs_clase/form2.php
@@ -13,8 +13,17 @@ $confirm = $_REQUEST["confirm"];
 $news = $_REQUEST["news"];
 
 
+
 // ! Si lo que viene después se evalúa falso, entramos en el
 // if, porque SE CUMPLE LA CONDICIÓN DADA
+
+//Filter var sirve para filtrar variables, es decir, en este caso tenemos email y FILTER_VALIDATE_EMAIL. Con la función filter_var, le tenemos que pasar una variable,
+//y la manera en que la va a filtrar. El resultado de filter_var es true o false. En este caso, lo que haya en email, va a comprobar que sea un email, es decir,
+//que tenga arrobas, .com....lo que sea. Si eso es así, es verdadero el filtro. En este caso, como tenemos la exclamación, significa que si no se cumple lo dicho
+//pasa al if. "Si no se cumple que sea un email correcto, va a decir que error en el email"
+//Para terminar, al igual que FILTER_VALIDATE_EMAIL, están otros filtros para validar que una cadena sea correcta, como FILTER_VALIDATE_BOOLEAN (que sea booleano)
+//FILTER_VALIDATE_FLOAT (que sea un número con decimales, es decir, que sea un valor con un punto en medio, y números a cada lado)
+//Existen más filtros, pero esos son algunos ejemplos
 
 if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     print "Error en el email";
