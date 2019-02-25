431dcd3bcae72ae2b1a7b1d76c94cda24a173253
unknown <adristudy@gmail.com>
2019-02-25 20:35:23 +0100

    asd

--- php/php_ejercicios/ej11.php
+++ php/php_ejercicios/ej11.php
@@ -1,17 +1,35 @@
 <?php
-  //ENUNCIADO ANTERIOR
-  //Crear una página que convierta una cantidad de dinero en euros a su correspondiente en dólares.
-  //Valide el campo.
-  
-  //Amplíe el ejercicio para insertar un desplegable con posible monedas a cambiar.
-  
-  $cantidad = $_REQUEST['cant'];
-  $equivADOLARES = 0.88;
-  
-  if (filter_var($cantidad, FILTER_VALIDATE_INT) || filter_var($cantidad, FILTER_VALIDATE_FLOAT)) {
-	  $result = $cant*$equivADOLARES;
-	  echo "$cant € son $resul $";
-  } else {
-	  echo "Error, el valor introducido debe de un número entero o decimal<br>Inténtelo de nuevo";
+	//ENUNCIADO EJERCICIO ANTERIOR
+	//Crear una página que convierta una cantidad de dinero en euros a su correspondiente en dólares.
+	//Valide el campo.
+
+	//ENUNCIADO ÉSTE EJERCICIO
+	//Amplíe el ejercicio para insertar un desplegable con posible monedas a cambiar.
+
+
+	//↓MAIN↓
+
+	//Constantes conversoras de monedas
+	define("convertDo", "1.14");
+	define("convertLi", "0.87");
+	define("convertYe", "126.25");
+
+	//Datos que nos vienen del formulario
+	$euros = trim(htmlspecialchars(strip_tags($_REQUEST["euros"]), ENT_QUOTES, "UTF-8"));
+	$se = trim(htmlspecialchars(strip_tags($_REQUEST["se"]), ENT_QUOTES, "UTF-8"));
+
+
+	//Conversión
+
+	if ($se == Do) {
+		$result = $euros*convertDo;
+		echo "$euros € son $result $";
+	} else if ($se == Li) {
+		$result = $euros*convertLi;
+		echo "$euros € son $result £";
+	} else {
+		$result = $euros*convertYe;
+		echo "$euros € son $result ¥"; 
 	}
+
 ?>
