af88ee0769dc5f67169eaace140828b5f1def78a
unknown <adristudy@gmail.com>
2019-02-26 00:20:16 +0100

    asdas

--- php/php_ejercicios/ej9_1.php
+++ php/php_ejercicios/ej9_1.php
@@ -5,13 +5,25 @@
   
   $num1 = trim(htmlspecialchars(strip_tags($_REQUEST["num1"]), ENT_QUOTES, "UTF-8"));
   $num2 = trim(htmlspecialchars(strip_tags($_REQUEST["num2"]), ENT_QUOTES, "UTF-8"));
-  $selection = trim(htmlspecialchars(strip_tags($_REQUEST["opeR"]), ENT_QUOTES, "UTF-8"));
-    
-  if ($selection == "suma") {
-	$result = $num1+$num2;
-	echo "$num1 + $num2 = $result";
+  $selectionS = trim(htmlspecialchars(strip_tags($_REQUEST["selectionS"]), ENT_QUOTES, "UTF-8"));
+  $selectionR = trim(htmlspecialchars(strip_tags($_REQUEST["selectionR"]), ENT_QUOTES, "UTF-8"));
+
+  if (!empty($num1) && !empty($num2) && is_numeric($num1) && is_numeric($num2)) {
+    if (!empty($selectionS) && !empty($selectionR)) {
+      echo "Para que se pueda realizar la operación, marque sólo una casilla por favor";
+    } elseif (empty($selectionS) && empty($selectionR)) {
+      echo "Para que se pueda realizar la operación, marque alguna casilla por favor";
+    } else {
+      if (!empty($selectionS)) {
+        $result = $num1+$num2;
+        echo "$num1 + $num2 = $result";
+      } else {
+        $result = $num1-$num2;
+        echo "$num1 - $num2 = $result";
+      }
+    }
+
   } else {
-	  $result = $num1-$num2;
-	  echo "$num1 - $num2 = $result";
-	}
+    echo "Los campos de número no pueden estar vacíos. Se deben rellenar SÓLO con números";
+  }
 ?>
