f525527e9af1d6dc0f781ac9bea7a260d18afafd
adriasir123 <adristudy@gmail.com>
2019-02-28 12:32:28 +0100

    sadsad

--- php/php_ejercicios/ej9_1.php
+++ php/php_ejercicios/ej9_1.php
@@ -2,7 +2,7 @@
   //Realice un formulario con dos campos de tipo texto para dos números y un radio group para dos
   //opciones, "suma" y "resta". Al pulsar el botón "Operar" debe calcular el resultado. Realice el mismo
   //ejemplo con checkbox y con select.
-  
+
   $num1 = trim(htmlspecialchars(strip_tags($_REQUEST["num1"]), ENT_QUOTES, "UTF-8"));
   $num2 = trim(htmlspecialchars(strip_tags($_REQUEST["num2"]), ENT_QUOTES, "UTF-8"));
   $selectionS = trim(htmlspecialchars(strip_tags($_REQUEST["selectionS"]), ENT_QUOTES, "UTF-8"));
