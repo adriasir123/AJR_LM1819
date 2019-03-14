69886bdac7d75f86f1c24d757af3594a6f7f8262
Alumno <adristudy@gmail.com>
2019-03-14 13:52:10 +0100

    SD

---
+++ php/pregunta08/archivo1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        //Realice el siguiente ejercicio:
+        //Muestra una consulta de todos los productos de la tabla productos mostrando el nombre completo del proveedor del producto.
+        //Además, muestre un mensaje indicando el precio total de todos los productos y la cantidad de productos. Añade un botón para
+        //insertar un producto en la base de datos. Muestra en el formulario todos los proveedores que existen en la tabla de proveedores.
+        
+    ?>
+</body>
+</html>
